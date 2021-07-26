43006a0360c2be5ae2a714c641cf048082097368
竹下大地 <pryf48fy@s.okayama-u.ac.jp>
2021-07-26 16:51:08 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import React from 'react';
 
 function App() {
-  return <div> <p> okumura-curb </p> </div>;
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am changed!")}}/>
+    </React.Fragment>
+  )
 }
 
 export default App;
